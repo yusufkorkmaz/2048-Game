087bf01be25ee5c18a0dc404dc4015376fd72236
Yusuf KORKMAZ <yusufkorkmazz98@hotmail.com>
2023-02-12 00:43:58 +0300

    Scroll added for mobile 📱

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let xDown;
+let yDown;
 let newGame;
 let score;
 let bestScore;
@@ -363,6 +365,58 @@ function gameOverControl() {
 
 gameInit();
 
+function anyMoveHappenedControl() {
+    if (anyMoveHappened) {
+        drawTheMove();
+        gameOverControl();
+    }
+}
+
+function getTouches(evt) {
+    return evt.touches || evt.originalEvent.touches;
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            arrowLeftPressed();
+        } else {
+            arrowRightPressed();
+        }
+    } else {
+        if (yDiff > 0) {
+            arrowUpPressed();
+        } else {
+            arrowDownPressed();
+        }
+    }
+    
+    xDown = null;
+    yDown = null;
+
+    anyMoveHappenedControl();
+};
+
+document.addEventListener('touchstart', handleTouchStart, false);
+
+document.addEventListener('touchmove', handleTouchMove, false);
+
 document.addEventListener('keydown', (event) => {
     var code = event.code;
     if (!delayingForAnimation) {
@@ -381,9 +435,6 @@ document.addEventListener('keydown', (event) => {
                 break;
         }
 
-        if (anyMoveHappened) {
-            drawTheMove();
-            gameOverControl();
-        }
+        anyMoveHappenedControl();
     }
 }, false);  
